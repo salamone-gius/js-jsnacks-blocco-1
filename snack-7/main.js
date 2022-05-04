1f4257a772a85f9929a2a6b8009be9a48ec11c0e
salamone-gius <salamone.gius@gmail.com>
2022-05-04 16:50:28 +0200

    snack-7

---
+++ snack-7/main.js
@@ -0,0 +1,8 @@
+// Generatore di "nomi cognomi" casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+// 1. creo una lista di nomi e una di cognomi
+let names = ["Giuseppe", "Giovanni", "Gianluca"];
+let surnames = ["Salamone", "Mura", "Parisi"];
+
+// 2. genero una lista di nomi e cognomi
+console.log(`${names[0]} ${surnames[0]}, ${names[1]} ${surnames[1]}, ${names[2]} ${surnames[2]}`);
